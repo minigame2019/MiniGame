369666816b092447a268e8e8098e4149fb2e03e6
ParadoxShi <shis2016th@163.com>
2019-07-18 21:20:10 +0800

    fix a bug in A* grid generator

---
+++ Assets/Scripts/Procedural/EndlessTerrian.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessTerrian : MonoBehaviour
+{
+    const float scale = 1f;
+
+    const float viewerMoveThresholdForChunkUpdate = 25f;
+    const float sqrViewerMoveThresholdForChunkUpdate = viewerMoveThresholdForChunkUpdate * viewerMoveThresholdForChunkUpdate;
+    Vector2 viewerPositionOld;
+
+    public LODInfo[] detailLevels;
+    public static float maxViewDst;
+
+    public Transform viewer;
+    public Material mapMaterial;
+
+    public static Vector2 viewerPosition;
+    static MapGenerator mapGenerator;
+    int chunkSize;
+    int chunksVisibleInViewDst;
+
+    static bool isFirstMesh = true;
+
+
+    Dictionary<Vector2, TerrianChunk> terrianChunkDictionary = new Dictionary<Vector2, TerrianChunk>();
+    static List<TerrianChunk> terrianChunksVisibleLastUpdate = new List<TerrianChunk>();
+
+    private void Start()
+    {
+        mapGenerator = FindObjectOfType<MapGenerator>();
+        viewer.transform.position = new Vector3(0, 20, 0);
+        maxViewDst = detailLevels[detailLevels.Length - 1].visibleDstThreshold;
+        chunkSize = MapGenerator.mapChunkSize - 1;
+        chunksVisibleInViewDst = Mathf.RoundToInt(maxViewDst / chunkSize);
+
+        UpdateVisibleChunks();
+    }
+
+    void Update()
+    {
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z) / scale;
+
+        if((viewerPositionOld - viewerPosition).sqrMagnitude > sqrViewerMoveThresholdForChunkUpdate){
+            viewerPositionOld = viewerPosition;
+            UpdateVisibleChunks();
+        }
+    }
+
+    void UpdateVisibleChunks()
+    {
+        for(int i = 0; i < terrianChunksVisibleLastUpdate.Count; i++)
+        {
+            terrianChunksVisibleLastUpdate[i].SetVisible(false);
+        }
+
+        terrianChunksVisibleLastUpdate.Clear();
+
+        int currentChunkCoordX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+        int currentChunkCoordY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+
+        for (int yOffset = -chunksVisibleInViewDst; yOffset <= chunksVisibleInViewDst; yOffset++)
+        {
+            for(int xOffset = -chunksVisibleInViewDst; xOffset <= chunksVisibleInViewDst; xOffset++)
+            {
+                Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+
+                if (terrianChunkDictionary.ContainsKey(viewedChunkCoord))
+                {
+                    terrianChunkDictionary[viewedChunkCoord].UpdateTerrianChunk();
+                }else
+                {
+                    terrianChunkDictionary.Add(viewedChunkCoord, new TerrianChunk(viewedChunkCoord, chunkSize, detailLevels, transform, mapMaterial));
+                }
+            }
+        }
+    }
+
+    public class TerrianChunk
+    {
+        GameObject meshObject;
+        Vector2 position;
+        Bounds bounds;
+
+        MapData mapData;
+        MeshRenderer meshRenderer;
+        MeshFilter meshFilter;
+        MeshCollider meshCollider;
+
+        LODInfo[] detailLevels;
+        LODMesh[] lodMeshes;
+
+        bool mapDataReceived;
+
+        int previousLODIndex = -1;
+
+
+        public TerrianChunk(Vector2 coord, int size, LODInfo[] detailLevels, Transform parent, Material material)
+        {
+            this.detailLevels = detailLevels;
+            position = coord * size;
+            bounds = new Bounds(position, Vector2.one * size);
+            Vector3 positionV3 = new Vector3(position.x, 0, position.y);
+
+            //meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            meshObject = new GameObject("Terrian Chunk");
+            meshRenderer = meshObject.AddComponent<MeshRenderer>();
+            meshFilter = meshObject.AddComponent<MeshFilter>();
+
+            meshCollider = meshObject.AddComponent<MeshCollider>();
+
+            meshRenderer.material = material;
+
+            meshObject.transform.position = positionV3 * scale;
+            meshObject.transform.parent = parent;
+            meshObject.transform.localScale = Vector3.one * scale;
+
+            SetVisible(false);
+
+            lodMeshes = new LODMesh[detailLevels.Length];
+            for(int i = 0; i < detailLevels.Length; i++)
+            {
+                lodMeshes[i] = new LODMesh(detailLevels[i].lod, UpdateTerrianChunk);
+            }
+
+            mapGenerator.RequestMapData(position, OnMapDataReceived);
+        }
+
+        void OnMapDataReceived(MapData mapData){
+            this.mapData = mapData;
+            mapDataReceived = true;
+            Texture2D texture = TextureGenerator.TextureFromColourMap(mapData.colourMap, MapGenerator.mapChunkSize, MapGenerator.mapChunkSize);
+            meshRenderer.material.mainTexture = texture;
+            UpdateTerrianChunk();
+          //  mapGenerator.RequestMeshData(mapData, onMeshDataReceived);
+
+          //  set grid map data here
+
+        }
+
+        void onMeshDataReceived(MeshData meshData)
+        {
+            meshFilter.mesh = meshData.CreateMesh();
+        }
+
+        public void UpdateTerrianChunk()
+        {
+            if (mapDataReceived)
+            {
+
+                float viewerDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance(viewerPosition));
+                bool visible = viewerDstFromNearestEdge <= maxViewDst;
+
+                if (visible)
+                {
+                    int lodIndex = 0;
+                    for (int i = 0; i < detailLevels.Length - 1; i++)
+                    {
+                        if (viewerDstFromNearestEdge > detailLevels[i].visibleDstThreshold)
+                        {
+                            lodIndex = i + 1;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    if (lodIndex != previousLODIndex)
+                    {
+                        LODMesh lodMesh = lodMeshes[lodIndex];
+                        if (lodMesh.hasMesh)
+                        {
+                            previousLODIndex = lodIndex;
+                            meshFilter.mesh = lodMesh.mesh;
+                            meshCollider.sharedMesh = lodMesh.mesh;
+
+                            if (isFirstMesh && position.x == 0 && position.y == 0)
+                            {
+                                AstarPath.active.Scan();
+                                isFirstMesh = false;
+                            }
+
+                        }
+                        else if (!lodMesh.hasRequestedMesh)
+                        {
+                            lodMesh.RequestMesh(mapData);
+                        }
+                    }
+
+                    terrianChunksVisibleLastUpdate.Add(this);
+                }
+
+                SetVisible(visible);
+            }
+        }
+
+        public void SetVisible(bool visible)
+        {
+            meshObject.SetActive(visible);
+        }
+
+        public bool isVisible()
+        {
+            return meshObject.activeSelf;
+        }
+    }
+
+    class LODMesh
+    {
+        public Mesh mesh;
+        public bool hasRequestedMesh;
+        public bool hasMesh;
+        int lod;
+        System.Action updateCallback;
+
+        public LODMesh(int lod, System.Action updateCallback)
+        {
+            this.lod = lod;
+            this.updateCallback = updateCallback;
+        }
+
+        void OnMeshDataReceived(MeshData meshData)
+        {
+            mesh = meshData.CreateMesh();
+            hasMesh = true;
+
+            updateCallback();
+        }
+
+        public void RequestMesh(MapData mapData)
+        {
+            hasRequestedMesh = true;
+            mapGenerator.RequestMeshData(mapData,lod, OnMeshDataReceived);
+        }
+    }
+
+    [System.Serializable]
+
+    public struct LODInfo
+    {
+        public int lod;
+        public float visibleDstThreshold;
+    }
+}
---
+++ Assets/Scripts/Procedural/MyGrid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class MyGridGraph : GridGraph
+{
+    public float[,] noiseMap;
+    public float seaLevel;
+    public override void RecalculateCell(int x, int z, bool resetPenalties = true, bool resetTags = true)
+    {
+        var node = nodes[z * width + x];
+        // Set the node's initial position with a y-offset of zero
+        node.position = GraphPointToWorld(x, z, 0);
+        int posx = node.position.x;
+        //int posx = node.position.x;
+        int posz = node.position.z;
+        node.Walkable = noiseMap[posx, posz] <= seaLevel? false : true;
+        node.Tag = 0;
+        // Store walkability before erosion is applied
+        // Used for graph updates
+        node.WalkableErosion = node.Walkable;
+    }
+}
