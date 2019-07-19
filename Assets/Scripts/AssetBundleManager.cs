3adeb34b5678c783b292b7a2ed479a216ae622ee
yf_wang <yf_wang16@163.com>
2019-07-19 16:47:54 +0800

    AssetBundleManager

---
+++ Assets/Scripts/AssetBundleManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.Networking;
+
+namespace MyAssetBundleManager
+{
+
+    public class AssetBundleManager : MonoBehaviour {
+
+        static AssetBundle manifestAB;
+        static AssetBundleManifest manifest;
+        private void init(){
+            manifestAB = AssetBundle.LoadFromFile("AssetBundles/AssetBundles");
+            manifest = manifestAB.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+        }
+
+        public void Start(){
+            print("AssetBundelMannager start");
+            init();
+        }
+
+        public static void loadDependencies(string asset_bundle_name){
+            //fresh();
+            string[] strs =  manifest.GetAllDependencies(asset_bundle_name);
+            print(asset_bundle_name + "  Depends on : ");
+            foreach (string name in strs)
+            {
+                print(name);
+                loadAssetBundle(name);
+            }
+        }
+
+        public static AssetBundle loadAssetBundle(string asset_bundle_name){
+            IEnumerable<AssetBundle>loaded_abs = AssetBundle.GetAllLoadedAssetBundles();
+            foreach(AssetBundle x in loaded_abs){
+                if(x.name == asset_bundle_name){
+                    loadDependencies(asset_bundle_name);
+                    return x;
+                }
+            }
+            AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync("AssetBundles/" + asset_bundle_name);
+            print("load : " + asset_bundle_name);
+            loadDependencies(asset_bundle_name);
+            return request.assetBundle;
+        }
+
+        public static GameObject loadByName(string asset_bundle_name, string asset_name){
+            AssetBundle ab = loadAssetBundle(asset_bundle_name);
+            GameObject obj = ab.LoadAsset<GameObject>(asset_name);
+            return obj;
+        }
+
+        public static void unloadAssetBundle(string asset_bundle_name, bool unload_all_dependencies = false){
+            print("unload: " + asset_bundle_name);
+            print(unload_all_dependencies);
+            IEnumerable<AssetBundle>loaded_abs = AssetBundle.GetAllLoadedAssetBundles();
+            foreach(AssetBundle x in loaded_abs){
+                if(x.name == asset_bundle_name){
+                    x.Unload(unload_all_dependencies);
+                    print("unload succees");
+                    break;
+                }
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/AssetBundleTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyAssetBundleManager;
+
+
+public class AssetBundleTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("Mouse 0");
+            GameObject x = AssetBundleManager.loadByName("cubewall.unity3d", "CubeWall");
+            Instantiate(x);
+            print("0 ok");
+            //AssetBundle.LoadFromFile("AssetBundles/share.unity3d");
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            print("Mouse 1");
+            AssetBundleManager.unloadAssetBundle("share.unity3d");
+            print("1 ok");
+        }
+        if (Input.GetMouseButtonDown(2))
+        {
+            print("Mouse 2");
+            IEnumerable<AssetBundle>loaded_abs = AssetBundle.GetAllLoadedAssetBundles();
+            foreach( AssetBundle x in loaded_abs){
+                print(x.name);
+            }
+            print("2 ok");
+            //AssetBundle.LoadFromFile("AssetBundles/share.unity3d");
+        }
+    }
+}
