1bfdc9bb1e4fe5639d126be54f873ba1330b0fe1
xiaoxd16 <xiaoxd16@163.com>
2019-07-22 19:01:23 +0800

    BackPack Add Remove

---
+++ Assets/GameCode/Item/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : InventoryItemBase, IDamageSource,IDurability
+{
+    [Range(1, 100)]
+    public int DamagePerHit = 20;
+    public int MaxDurability = 100;
+    public int CurrentDurability = 100;
+
+    public int GetDamagePerHit()
+    {
+        return DamagePerHit;
+    }
+
+    public override void OnUse()
+    {
+        base.OnUse();
+    }
+
+    public void TakeDurability(int num)
+    {
+        this.CurrentDurability -= num;
+        if (this.CurrentDurability <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Resources/Test.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        Transform content = this.transform.GetChild(0).GetChild(0);
+
+        content.GetComponent<RectTransform>().sizeDelta = new Vector2(200, 300);
+        GridLayoutGroup g = content.GetComponent<GridLayoutGroup>();
+        g.cellSize = new Vector2(80, 80);
+        g.spacing = new Vector2(10, 10);
+        Debug.Log(g.padding);
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject tem = Instantiate(Resources.Load("Slot_1")) as GameObject;
+            tem.transform.parent = content;
+            //tem.transform.localPosition = new Vector3(40, -40, 0) + new Vector3(80, 0, 0) * i;
+            tem.transform.localScale = new Vector3(1, 1, 1);
+        }*/
+        /*
+       Transform content = this.transform.GetChild(0).GetChild(0);
+        var r = this.GetComponent<RectTransform>();
+        int w =  int.Parse(r.sizeDelta.x.ToString());
+        var rt = content.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(w, 80);
+        for (int i = 0; i< 3; i++) {
+            GameObject tem = Instantiate(Resources.Load("Slot_1")) as GameObject;
+            tem.transform.parent = content;
+            tem.transform.localPosition = new Vector3(40,-40,0) + new Vector3(80, 0, 0) * i;
+            tem.transform.localScale = new Vector3(1, 1, 1);            
+            Debug.Log(tem.transform.localPosition);
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
