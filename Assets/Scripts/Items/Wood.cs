85732e74f52882aad7ddadab545eb572ea83ccb7
Junchen Cai <caleden@163.com>
2019-07-22 20:10:43 +0800

    cutTree
    用斧头砍树、捡起木头等。

---
+++ Assets/Scripts/Items/Wood.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wood : InventoryItemBase
+{
+    public override void OnUse()
+    {
+        // TODO: Do something with the object.... next tutorial!
+        base.OnUse();
+    }
+}
