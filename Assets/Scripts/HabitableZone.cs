6c145a4a220e83d73ce44bd96989f7c1f0f89c5b
yf_wang <yf_wang16@163.com>
2019-07-22 20:38:09 +0800

    有瑕疵的宜居带可视化

---
+++ Assets/Scripts/HabitableZone.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HabitableZoneManager
+{
+    
+public class HabitableZone : MonoBehaviour
+{
+    static public float left_bound;
+    static public float right_bound;
+    static public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        left_bound = -100f;
+        right_bound = 100f;
+        speed = 0.1f;
+    }
+
+    public float distanceWith(float x){
+        if(x > left_bound && x < right_bound){
+            return -1;
+        }
+        if(x <= left_bound){
+            return left_bound - x;
+        }
+        return x - right_bound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        left_bound += speed;
+        right_bound += speed;
+    }
+}
+
+}
