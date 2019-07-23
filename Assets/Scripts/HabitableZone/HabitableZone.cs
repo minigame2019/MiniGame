81ffc78ee4c28a71dd1a7d1197e82634bbbb43b6
yf_wang <yf_wang16@163.com>
2019-07-23 22:43:42 +0800

    宜居带

---
+++ Assets/Scripts/HabitableZone/HabitableZone.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HabitableZoneManager
+{
+    
+public class HabitableZone : MonoBehaviour
+{
+    static public float left_bound = -500f;
+    static public float right_bound = 500f;
+    static public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // 0 : 内
+    // + ：热带及距离
+    // - : 冷带及距离
+    public float distanceWith(float x){
+        if(x >= left_bound && x <= right_bound){
+            return 0;
+        }
+        if(x < left_bound){
+            return x - left_bound;
+        }
+        return x - right_bound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        left_bound += speed;
+        right_bound += speed;
+    }
+}
+
+}
---
+++ Assets/Scripts/HabitableZone/shelterMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HabitableZoneManager;
+
+public class shelterMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void moveToX(float distance){
+        this.transform.Translate(new Vector3(distance, 0, 0));
+    }
+
+    public void moveToZ(float distance){
+        this.transform.Translate(new Vector3(distance, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveToX(HabitableZone.speed);
+    }
+}
